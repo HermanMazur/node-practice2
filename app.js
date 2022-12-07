0f20ed1b841adae8e37ea682067d5dc65fd6b599
color <95304946+HermanMazur@users.noreply.github.com>
2022-12-07 14:43:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
-const express = require("express");    
+// const express = require("express");
+
+// const books = require("./books.json");
+
+// const app = express();
+
+// app.set("json spaces", 8)
+
+// app.get("/books", (req, res) => {
+//     const databaseResponse = null;
+//     // res.json(databaseResponse);
+//     // res.send(databaseResponse);
+//         res.json(books)
+// });
+
+// app.listen(3000);
+
+// // app - ours web server
+// // app.listen(3000, () => console.log("Server running"));
+
+
+
+// 2////////////////2////////////22222222222222222222///////middleware!!!!!
+
+const express = require("express");
 
 const books = require("./books.json");
 
 const app = express();
 
-app.get("/books", (req, res) => {
-    res.send(books)
+app.get("/products", (req, res) => {
+    res.json([]);
 });
 
+app.get("/books", (req, res) => {
+    res.json(books);
+})
+
 app.listen(3000);
-
-// app - ours web server
-// app.listen(3000, () => console.log("Server running"));
-
 
