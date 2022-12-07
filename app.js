96f443e3f8259311a776f2d155d16573fac6afcd
color <95304946+HermanMazur@users.noreply.github.com>
2022-12-07 13:17:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+// const { response, request } = require("express");
 const express = require("express");    
 
 const app = express(); // app - ours web server
 
-app.listen(3000, () => console.log("server running"));
+app.get("/", (request, response) => {
+    response.send("<h2>Home page</h2>")
+});
 
+app.get("/contacts", (request, response) => {
+    console.log(request.url);
+    console.log(request.method);
+    response.send("<h2>Contacts page</h2>")
+});
+
+app.listen(3000, () => console.log("Server running"));
 
 
