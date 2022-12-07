43196c153f4fb08a187b93a3fbc27548d91e9f2f
color <95304946+HermanMazur@users.noreply.github.com>
2022-12-07 15:18:35 +0200

    add format.log

--- app.js
+++ app.js
@@ -23,27 +23,36 @@
 // 2////////////////2////////////22222222222222222222///////middleware!!!!!
 
 const express = require("express");
+const fs = require("fs/promises");
+const moment = require("moment");
 
 const books = require("./books.json");
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("first middleware");
+app.use(async (req, res, next) => {
+    const { method, url } = req;
+    const data = moment().format("YYYY-MM-DD_hh:mm:ss");
+    await fs.appendFile("./public/server.log", `\n${method} ${url} ${data}`);
     next();
 });
 
-app.use((req, res, next) => {
-    console.log("second middleware");
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log("first middleware");
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log("second middleware");
+//     next();
+// });
 
 
-app.get("/products", (req, res) => {
+app.get("/products", async (req, res) => {
     res.json([]);
 });
 
-app.get("/books", (req, res) => {
+app.get("/books", async (req, res) => {
     res.json(books);
 })
 
